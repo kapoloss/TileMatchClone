da79a41ffafa1eb2276da7817d1902c1a0d27e67
kapoloss <67629032+kapoloss@users.noreply.github.com>
2023-12-21 01:50:09 +0300

    Build bugs fixed

--- Assets/Scripts/LevelMaker.cs
+++ Assets/Scripts/LevelMaker.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -316,3 +317,4 @@ public class LevelMaker : Singleton<LevelMaker>
 // {
 //     public List<int> piecesActivityData ;
 // }
+#endif
